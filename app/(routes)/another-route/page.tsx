8b2f13346f1d407f4d67b5f22b0040441c3fa5b3
Aydin Elhan <aydinmate@protonmail.com>
2023-08-19 12:33:57 +1000

    added two routes

---
+++ app/(routes)/another-route/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface AnotherRoutePageProps {
+ 
+}
+
+const AnotherRoutePage: React.FC<AnotherRoutePageProps> = ({}) => {
+  return (
+    <div>
+      hello another route
+    </div>
+  );
+}
+
+export default AnotherRoutePage;
---
+++ app/(routes)/test-route/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface TestRoutePageProps {
+ 
+}
+
+const TestRoutePage: React.FC<TestRoutePageProps> = ({}) => {
+  return (
+    <div>
+      hello world test route
+    </div>
+  );
+}
+
+export default TestRoutePage;
