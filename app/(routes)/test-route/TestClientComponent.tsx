f72d9d94a8dc38e6b0dd20fb1bac425882abf0d9
Aydin Elhan <aydinmate@protonmail.com>
2023-08-19 16:31:08 +1000

    added product names in the next page to test fetching data.

--- app/(routes)/test-route/TestClientComponent.tsx
+++ app/(routes)/test-route/TestClientComponent.tsx
@@ -1,32 +1,43 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+interface Product {
+  id: string;
+  name: string;
+}
 
 interface TestClientComponentProps {
- 
+  products: Product[];
 }
 
-const TestClientComponent: React.FC<TestClientComponentProps> = ({}) => {
+const TestClientComponent: React.FC<TestClientComponentProps> = ({
+  products,
+}) => {
+  const [counter, setCounter] = useState(1);
 
-    const [counter, setCounter] = useState(1)
+  const addHandler = () => {
+    setCounter(counter + 1);
+  };
 
-    const addHandler = () => {
-        setCounter(counter + 1)
-    }
-
-
-    const subtractHandler = () => {
-        setCounter(counter - 1)
-    }
-
+  const subtractHandler = () => {
+    setCounter(counter - 1);
+  };
 
   return (
-    <div>
-      <button onClick={addHandler}>add</button>
-      <button onClick={subtractHandler}>subtract</button>
-      <p>{counter}</p>
-    </div>
+    <>
+      <div>
+        <button onClick={addHandler}>add</button>
+        <button onClick={subtractHandler}>subtract</button>
+        <p>{counter}</p>
+      </div>
+      {products.length > 0
+        ? products.map((product: Product) => (
+            <p key={product.id}>{product.name}</p>
+          ))
+        : "products not found"}
+    </>
   );
-}
+};
 
 export default TestClientComponent;
--- app/(routes)/test-route/page.tsx
+++ app/(routes)/test-route/page.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import TestClientComponent from './TestClientComponent';
+import prismadb from '@/lib/prismadb';
 
 interface TestRoutePageProps {
  
 }
 
-const TestRoutePage: React.FC<TestRoutePageProps> = ({}) => {
+
+
+const TestRoutePage: React.FC<TestRoutePageProps> = async ({}) => {
+
+
+const products = await prismadb.product.findMany(
+  {
+    where: {
+      storeId: "319e42e8-2651-480f-85ff-3ed22b05ab09"
+    },
+    select: {
+      name: true,
+      id: true
+    }
+  }
+)
+
   return (
     <div>
       hello world test route
-      <TestClientComponent />
+      <TestClientComponent products={products}/>
     </div>
   );
 }
