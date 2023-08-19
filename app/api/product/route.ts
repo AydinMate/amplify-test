18f18adfca94301461c4cf5c6308a60463f5f836
Aydin Elhan <aydinmate@protonmail.com>
2023-08-19 17:09:00 +1000

    created clean api product

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -6,28 +6,10 @@ export async function GET(
   req: Request,
 ) {
   try {
-    const { searchParams } = new URL(req.url);
-    const categoryId = searchParams.get("categoryId") || undefined;
-    const sizeId = searchParams.get("sizeId") || undefined;
-    const colourId = searchParams.get("colourId") || undefined;
-    const isFeatured = searchParams.get("isFeatured");
-
-
 
     const products = await prismadb.product.findMany({
       where: {
         storeId: "319e42e8-2651-480f-85ff-3ed22b05ab09",
-        categoryId,
-        colourId,
-        sizeId,
-        isFeatured: isFeatured ? true : undefined,
-        isArchived: false,
-      },
-      include: {
-        images: true,
-        category: true,
-        colour: true,
-        size: true,
       },
       orderBy: {
         createdAt: "desc",
